fafca59927546a64639f544e2eb9027dbf750830
BryanBalantes <bryanbalantes22@gmail.com>
2026-02-09 19:00:46 +0800

    Java Spring Boot Practice

---
+++ logbook-app/logbook-app/src/test/java/com/commercial/logbook_app/LogbookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commercial.logbook_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
